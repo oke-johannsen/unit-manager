6cae3e2b8682fe47f4a1f807916aa090fe0fc19d
Oke Johannsen <oke.jo26@gmail.com>
2023-10-03 09:09:01 +0200

    fix secret leak

--- .deploy/mup.js
+++ .deploy/mup.js
@@ -2,9 +2,9 @@ module.exports = {
   servers: {
     one: {
       // TODO: set host address, username, and authentication method
-      host: "5.181.51.205",
-      username: "root",
-      password: "7yU3vc6MHfFW7Jr",
+      host: "HOST",
+      username: "USR",
+      password: "PWD",
       // pem: './path/to/pem'
       // or neither for authenticate from ssh-agent
     },
@@ -27,9 +27,8 @@ module.exports = {
     env: {
       // TODO: Change to your app's url
       // If you are using ssl, it needs to start with https://
-      ROOT_URL: "https://v2202309206749239350.megasrv.de",
-      MONGO_URL:
-        "mongodb://eden-app:LdCcq6CdtbZRmsfuX@SG-eugalaxycluster-38731.servers.mongodirector.com:27017,SG-eugalaxycluster-38732.servers.mongodirector.com:27017/tf11-eu-meteorapp-com?replicaSet=RS-eugalaxycluster-0&tls=true&tlsAllowInvalidCertificates=true",
+      ROOT_URL: "URL",
+      MONGO_URL: "mongodb+srv://USR:PWD@MONGODB/",
     },
 
     docker: {
@@ -40,17 +39,4 @@ module.exports = {
     // You might need to disable it on CI servers
     enableUploadProgressBar: true,
   },
-
-  // (Optional)
-  // Use the proxy to setup ssl or to route requests to the correct
-  // app when there are several apps
-
-  // proxy: {
-  //   domains: 'mywebsite.com,www.mywebsite.com',
-
-  //   ssl: {
-  //     // Enable Let's Encrypt
-  //     letsEncryptEmail: 'email@domain.com'
-  //   }
-  // }
 };
