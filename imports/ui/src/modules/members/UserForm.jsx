bfbd9274f7f9ff8b03c954b55eda1ab198438e13
mando <oke.johannsen@protonmail.com>
2023-10-12 21:13:40 +0200

    squad positions

--- imports/ui/src/modules/members/UserForm.jsx
+++ imports/ui/src/modules/members/UserForm.jsx
@@ -261,14 +261,17 @@ const UserForm = ({ userId, closeModal, forms, setForms, submitForms }) => {
             <Select
               disabled={securityClearance < 3}
               optionFilterProp="label"
+              options={[
+                { key: 1, label: "1", value: 1 },
+                { key: 2, label: "2", value: 2 },
+                { key: 3, label: "3", value: 3 },
+                { key: 4, label: "4", value: 4 },
+                { key: 5, label: "5", value: 5 },
+                { key: 6, label: "6", value: 6 },
+                { key: 7, label: "Strap", value: 7 },
+              ]}
               showSearch
-            >
-              <Select.Option value="Trupp-Soldat">Trupp-Soldat</Select.Option>
-              <Select.Option value="Stv. Truppführer">
-                Stv. Truppführer
-              </Select.Option>
-              <Select.Option value="Truppführer">Truppführer</Select.Option>
-            </Select>
+            ></Select>
           </Form.Item>
         </Col>
       </Row>
